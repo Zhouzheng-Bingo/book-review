a93982eb5636fa9f80c471d975a97ce0f7b6ee0f
Bingo <204784224@qq.com>
2019-01-10 12:04:24 +0000

    third time

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
 	before_action :find_book
+	before_action :find_review, only: [:edit, :update, :destroy]
+	before_action :authenticate_user!, only: [:new, :edit]
 
 	def new
 		@review = Review.new
@@ -8,13 +10,29 @@ class ReviewsController < ApplicationController
 	def create
 		@review = Review.new(review_params)
 		@review.book_id = @book.id
-		
+		@review.user_id = current_user.id
 
 		if @review.save
 			redirect_to book_path(@book)
 		else
 			render 'new'
 		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @review.update(review_params)
+			redirect_to book_path(@book)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@review.destroy
+		redirect_to book_path(@book)
 	end
 
 	private
@@ -25,6 +43,10 @@ class ReviewsController < ApplicationController
 
 		def find_book
 			@book = Book.find(params[:book_id])
+		end
+
+		def find_review
+			@review = Review.find(params[:id])
 		end
 
 end
